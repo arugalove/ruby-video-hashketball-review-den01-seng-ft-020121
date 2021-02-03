110c4551e2ea54dc6659d7acbf0d716d7ca715e9
Steven Yi <steven.yi@berkeley.edu>
2021-02-03 07:26:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -148,9 +148,8 @@ def shoe_size(player_name)
 end
 
 def find_team(team_name)
-  (
-    game_hash.filter do | home_or_away, team |
-      team[:team_name] == team_name
+  game_hash.filter do | home_or_away, team |
+    team[:team_name] == team_name
   end
 end
 
